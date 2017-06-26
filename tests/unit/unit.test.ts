2860316f4325a46e7bd3ab63cf29f1611396b535
davidssousa <david.sousa@msn.com>
2017-06-26 00:44:29 -0300

    create tests integration

---
+++ tests/unit/unit.test.ts
@@ -0,0 +1,27 @@
+import {testDouble, expect } from './config/helpers'
+
+describe('Testes de Unidade do Controller', () => {
+    describe ('Metodo Create', () => {
+        it ('Deve criar um novo Usuario', () => {
+            
+        });
+    });
+
+    describe ('Metodo Update', () => {
+        it ('Deve atualizar um Usuario', () => {
+            
+        });
+    });
+
+    describe ('Metodo GET Users', () => {
+        it ('Deve retornar um lista com todos os Usuario', () => {
+            
+        });
+    });
+
+    describe ('Metodo Delete', () => {
+        it ('Deve excluir um Usuario', () => {
+            
+        });
+    });
+})
--- tests/util/unit.test.ts
+++
@@ -1,9 +0,0 @@
-import {testDouble, expect } from './config/helpers'
-
-describe('Testes de Unidade do Controller', () => {
-    describe ('Method Create', () => {
-        it ('Deve criar um novo Usuario', () => {
-            
-        });
-    });
-})
